6152512e86d851510a26232b96c965d8421b51a3
Colin Coleman <colin.coleman@kontrollsmart.com>
2020-04-03 01:40:17 +0200

    swap gosu for su-exec

--- docker/sonarqube-aws-env/entrypoint.sh
+++ docker/sonarqube-aws-env/entrypoint.sh
@@ -4,4 +4,4 @@ chown -R sonarqube:sonarqube $SONARQUBE_HOME
 if [ -z "$AWS_REGION" ] && [ -z "$AWS_DEFAULT_REGION" ]; then
 export AWS_REGION="eu-west-1"
 fi
-exec gosu sonarqube ./start-with-params.sh
+exec su-exec sonarqube ./start-with-params.sh
