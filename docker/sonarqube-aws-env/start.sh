d11347cb6af028b41a36715aef99746d3f60f40f
Colin Coleman <colin.coleman@kontrollsmart.com>
2020-04-22 05:10:11 +0200

    Attempt to fix virtual memory errors check

--- docker/sonarqube-aws-env/start.sh
+++ docker/sonarqube-aws-env/start.sh
@@ -6,4 +6,5 @@ java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.search.javaAdditionalOpts="$SONARQUBE_SEARCH_JVM_OPTS" \
   "$@"
