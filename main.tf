54c3ec8dafcc406bcaa88755978f2e848ca27c86
Colin Coleman <colin.coleman@kontrollsmart.com>
2020-05-26 17:17:28 +0200

    use test version of ecs-fargate module

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_lb_listener" "main" {
 }
 
 module "sonarqube_service" {
-  source     = "github.com/nsbno/terraform-aws-ecs-fargate?ref=e6bf17f"
+  source     = "github.com/nsbno/terraform-aws-ecs-fargate?ref=7732048"
   cluster_id = aws_ecs_cluster.cluster.id
   health_check = {
     port    = "traffic-port"
