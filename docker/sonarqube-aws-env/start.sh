5364bb51bc2f4ab2615393dd699de83616dfff7e
stekern <stekern@users.noreply.github.com>
2021-05-05 18:11:51 +0200

    Add new flag to SonarQube

--- docker/sonarqube-aws-env/start.sh
+++ docker/sonarqube-aws-env/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./post-init.sh &
 java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.forceAuthentication=true \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
